d1031f6e49ac5531040c8fcc7742d40442aca2b7
Lisa Herzberg <lisamarieherzberg@gmail.com>
2021-01-25 17:21:51 -0700

    add some more logic for whose turn it it

--- index.js
+++ index.js
@@ -20,28 +20,54 @@ const createUser = (id, name) => {
         id,
         name,
         ready: false,
-        userNum:  user++
+        userNum:  user++,
+        turn: false
     }
 };
 
 let connectedUsers = [];
 
-const colors = [
-    '#ecdb54',
-    '#e34132',
-    '#6ca0dc',
-    '#944743',
-    '#dbb2d1',
-    '#ec9787',
-    '#00a68c',
-    '#645394',
-    '#6c4f3d',
-    '#ebe1df',
-    '#bc6ca7',
-    '#bfd833',
+const pictures = [
+    'https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=976&q=80',
+    'https://images.unsplash.com/photo-1497206365907-f5e630693df0?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1100&q=80',
+    'https://images.unsplash.com/photo-1525382455947-f319bc05fb35?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1114&q=80',
+    'https://images.unsplash.com/photo-1475372674317-8003c861cb6a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1567&q=80',
+    'https://images.unsplash.com/photo-1527161153332-99adcc6f2966?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1649&q=80',
+    'https://images.unsplash.com/photo-1553558888-6440b4097908?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1100&q=80',
+    'https://images.unsplash.com/photo-1576512259505-bd5cd2a70ff8?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1100&q=80',
+    'https://images.unsplash.com/photo-1604521221077-54a0dc93612a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1148&q=80',
+    'https://images.unsplash.com/photo-1605337159501-cc841b9cf6e4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1100&q=80',
+    'https://images.unsplash.com/photo-1562649625-f5d82689958a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1100&q=80',
+    'https://images.unsplash.com/photo-1610109316440-2bc5ca557e9e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1134&q=80',
+    'https://images.unsplash.com/photo-1588421034962-3331763af8a0?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1145&q=80',
+    'https://images.unsplash.com/photo-1560659245-ea431e0154a9?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1100&q=80',
+    'https://images.unsplash.com/photo-1550853024-fae8cd4be47f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1107&q=80'
 ];
 
-const randomNumber = 6;
+const randomNumber = .35234523453;
+
+let playerGoesFirst = 0;
+
+
+const shuffle = (array, number) => {
+    let currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(number * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
 
 io.on("connection", socket => {
 
@@ -72,14 +98,14 @@ io.on("connection", socket => {
         for (let i = 0; i < options / 2; i++) {
             const firstOption = {
                 id: 2 * i,
-                colorId: i,
-                color: colors[i],
+                pictureId: i,
+                picture: pictures[i],
                 flipped: false,
             };
             const secondOption = {
                 id: 2 * i + 1,
-                colorId: i,
-                color: colors[i],
+                pictureId: i,
+                picture: pictures[i],
                 flipped: false,
             };
 
@@ -87,12 +113,29 @@ io.on("connection", socket => {
             newGame.push(secondOption);
         }
 
-        const shuffledGame = newGame.sort(() => randomNumber);
-        socket.emit('newGame', shuffledGame)
+        const shuffledGame = shuffle(newGame, randomNumber);
+        connectedUsers.forEach((user, index) => {
+            if(user.id === connectedUsers[0].id) {
+                connectedUsers[index].turn = true;
+            }
+        });
+        io.emit('users', connectedUsers);
+        io.emit('newGame', shuffledGame)
     });
 
     socket.on('cardFlipped', (data) => {
         io.emit('cardFlipped', data);
+    });
+
+
+    socket.on('nextPlayer', (indexOfUser) => {
+        connectedUsers[indexOfUser].turn = true;
+        if(indexOfUser > 0){
+            connectedUsers[indexOfUser -1].turn = false;
+        } else {
+            connectedUsers[connectedUsers.length - 1].turn = false;
+        }
+        io.emit('users', connectedUsers);
     })
 
 });
